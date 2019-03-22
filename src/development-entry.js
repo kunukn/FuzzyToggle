41ae9aaab30feb9f7ba38cfb7ec74b77cd8d5346
Kunuk Nykjær <kunyk@dfds.com>
2019-03-22 15:58:06 +0100

    temp

--- src/development-entry.js
+++ src/development-entry.js
@@ -1,3 +1,22 @@
-import fuzzyToggle from 'src/fuzzyToggle';
+import FuzzyToggle from 'src/FuzzyToggle';
 
-fuzzyToggle.run();
+var toggle = new FuzzyToggle();
+
+var log = console.log.bind(console);
+
+function onComplete(arg){
+  log(arg);
+  log('** example done');
+  log('**  example start 2');
+  toggle.play({ duration: 300, update: log, complete: onComplete2 });  
+}
+
+function onComplete2(arg){
+  log(arg);
+  log('** example done 2');
+}
+
+log('**  example start');
+toggle.play({ duration: 500, update: log, complete: onComplete });
+
+//setTimeout(() => cancel(range => log('cancelled', range)), 300);
--- src/fuzzyToggle/index.js
+++ src/fuzzyToggle/index.js
@@ -1,9 +1,81 @@
-function fuzzyToggle(){
+let root = typeof window !== 'undefined'
+  ? window
+  : global;
+
+let rAF = root.requestAnimationFrame
+  ? requestAnimationFrame.bind(root)
+  : function (callback) {
+    setTimeout(callback, 16)
+  };
+
+let getNow = () => new Date().getTime();
+
+class FuzzyToggle(){
+
+  constructor({update,complete duration = 300} = {}){
+    this.update = update;
+    this.complete = complete;
+    this.duration = duration;
+  }
+
+  
 }
 
-fuzzyToggle.run = function(){
-    let root = document.getElementById('root');
-    if(root) root.textContent = 'Hello';
-}
+function FuzzyToggle({update,complete duration = 300} = {}) {
 
-export default fuzzyToggle;
+  var prevRange;
+  var cancelled = false;
+  var startTime;
+
+  var attrs = args || {};
+
+  var update = attrs.update;
+  var complete = attrs.complete;
+  var duration = attrs.duration != null
+    ? + (attrs.duration)
+    : 300;
+  var ensureLast = attrs.ensureLast != null
+    ? attrs.ensureLast
+    : true;
+
+  function getCurrent(startTime) {
+    var now = getNow();
+    var elapsedTime = now - startTime;
+    return {
+      startTime: startTime,
+      now: now,
+      elapsedTime: elapsedTime,
+      range: Math.min(elapsedTime / duration, 1)
+    };
+  }
+
+  this.play = function play() {
+    if (cancelled) 
+      return;
+    
+    var current = getCurrent(startTime);
+
+    if (current.elapsedTime >= duration) {
+      ensureLast && prevRange !== current.range && update && update(1);
+      complete && complete(current);
+    } else {
+      update && update(current.range);
+      prevRange = current.range;
+      rAF(play);
+    }
+  }
+
+  this.startTime = getNow();
+
+  if (duration > 0) {
+    update && update(0);
+    rAF(play);
+  }
+
+  this.cancel = function cancel(callback) {
+    cancelled = true;
+    callback && callback(getCurrent(startTime));
+  }
+};
+
+export default FuzzyToggle;
